b7919a83ccd1e5d68f31d069bad31a26d4b1f2da
loitdbw <129943395+loitdbw@users.noreply.github.com>
2023-05-31 15:40:18 +0700

    refactor the code more clean

--- craw-web-without-asyncio.py
+++ craw-web-without-asyncio.py
@@ -5,54 +5,60 @@ from lxml import etree
 import time
 
 
+class RequestFailedException(Exception):
+    pass
+
+
 def get_href_values_xpath(url, xpath_expression, headers=None):
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        tree = etree.HTML(response.content)
-        href_list = tree.xpath(f'{xpath_expression}//a/@href')
-        return href_list
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+    response.raise_for_status()  # Will raise an exception if the request failed
+
+    tree = etree.HTML(response.content)
+    href_list = tree.xpath(f'{xpath_expression}//a/@href')
+
+    return href_list
 
 
 def get_html_content(url, headers=None):
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.text
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+    response.raise_for_status()
+
+    return response.text
 
 
 def process_links(base_url, href_list, headers):
     data_list = []
     for index, href in enumerate(href_list, start=1):
         url = base_url + href
-        if "https://www.energy.gov.au/rebates" in url:
-            html_content = get_html_content(url, headers=headers)
-            if html_content:
-                data = {
-                    "url": url,
-                    "html_content": html_content,
-                    "numeric_value": index,  # Set numeric value
-                }
-                data_list.append(data)
-                print(f"Processed link {index}: {url}")
-        print()
+        if "https://www.energy.gov.au/rebates" not in url:
+            print(
+                f"Skipped link {index}: {href} (Doesn't contain 'https://www.energy.gov.au/rebates')")
+            continue
+
+        html_content = get_html_content(url, headers=headers)
+
+        data = {
+            "url": url,
+            "html_content": html_content,
+            "numeric_value": index,  # Set numeric value
+        }
+        data_list.append(data)
+        print(f"Processed link {index}: {url}")
+
     return data_list
 
 
 def write_data_to_json(data_list):
-    if data_list:
-        result_dir = "results"
-        os.makedirs(result_dir, exist_ok=True)
-        file_name = os.path.join(result_dir, "all_data.json")
-        with open(file_name, "w") as json_file:
-            json.dump(data_list, json_file, indent=4)
-            print(f"\nAll data written to JSON file: {file_name}")
-    else:
+    if not data_list:
         print("\nNo data to write.")
+        return
+
+    result_dir = "results"
+    os.makedirs(result_dir, exist_ok=True)
+    file_name = os.path.join(result_dir, "all_data.json")
+    with open(file_name, "w") as json_file:
+        json.dump(data_list, json_file, indent=4)
+        print(f"\nAll data written to JSON file: {file_name}")
 
 
 if __name__ == '__main__':
@@ -81,14 +87,13 @@ if __name__ == '__main__':
 
     xpath_expression = '//*[@id="content"]'
 
-    href_list = get_href_values_xpath(
-        source_link, xpath_expression, headers=headers)
-    if href_list:
-        print("Href values extracted using XPath:")
+    try:
+        href_list = get_href_values_xpath(
+            source_link, xpath_expression, headers=headers)
         data_list = process_links(base_url, href_list, headers=headers)
         write_data_to_json(data_list)
-    else:
-        print("\nNo data to process.")
+    except RequestFailedException:
+        print(f"Request to {source_link} failed")
 
     end_time = time.time()
     execution_time = end_time - start_time
--- craw_web_asyncio.py
+++ craw_web_asyncio.py
@@ -8,59 +8,56 @@ import time
 
 async def get_href_values_xpath(session, url, xpath_expression):
     async with session.get(url) as response:
-        if response.status == 200:
-            tree = etree.HTML(await response.text())
-            href_list = tree.xpath(f'{xpath_expression}//a/@href')
-            return href_list
-        else:
-            print(f"Error: {response.status}")
-            return None
+        response.raise_for_status()  # Will raise an exception if the request failed
+        tree = etree.HTML(await response.text())
+        href_list = tree.xpath(f'{xpath_expression}//a/@href')
+        return href_list
 
 
 async def get_html_content(session, url):
     async with session.get(url) as response:
-        if response.status == 200:
-            return await response.text()
-        else:
-            print(f"Error: {response.status}")
-            return None
+        response.raise_for_status()
+        return await response.text()
 
 
-async def process_links(base_url, href_list, session):
-    data_list = []
-
-    for href in href_list:
-        url = base_url + href
-        if "https://www.energy.gov.au/rebates" in url:
-            html_content = await get_html_content(session, url)
-            if html_content:
-                data = {
-                    "url": url,
-                    "html_content": html_content
-                }
-                data_list.append(data)
-                print(f"Processed link: {url}")
-        else:
-            print(
-                f"Skipped link: {href} (Doesn't contain 'https://www.energy.gov.au/rebates')")
-
-    if data_list:
-        result_dir = "results-asyncio"
-        os.makedirs(result_dir, exist_ok=True)
-        file_name = os.path.join(result_dir, "all_data.json")
-        with open(file_name, "w") as json_file:
-            json.dump(data_list, json_file, indent=4)
-            print(f"\nAll data written to JSON file: {file_name}")
-    else:
-        print("\nNo data to write.")
+async def process_link(session, base_url, href):
+    url = base_url + href
+    if "https://www.energy.gov.au/rebates" in url:
+        html_content = await get_html_content(session, url)
+        data = {
+            "url": url,
+            "html_content": html_content
+        }
+        print(f"Processed link: {url}")
+        return data
+    return None
 
 
-async def crawl_website(base_url, source_link, headers=None, xpath_expression=None):
+async def process_links(session, base_url, href_list):
+    tasks = [process_link(session, base_url, href) for href in href_list]
+    return await asyncio.gather(*tasks)
+
+
+async def write_data_to_json(data_list):
+    result_dir = "results-asyncio"
+    os.makedirs(result_dir, exist_ok=True)
+    file_name = os.path.join(result_dir, "all_data.json")
+    with open(file_name, "w") as json_file:
+        json.dump(data_list, json_file, indent=4)
+        print(f"\nAll data written to JSON file: {file_name}")
+
+
+async def crawl_website(base_url, source_link, headers=None, xpath_expression=None, num_pages=3):
     async with aiohttp.ClientSession(headers=headers) as session:
-        href_list = await get_href_values_xpath(session, source_link, xpath_expression)
-        if href_list:
-            print("Href values extracted using XPath:")
-            await process_links(base_url, href_list, session)
+        for page in range(0, num_pages+1):
+            url = f"{source_link}&page={page}"
+            href_list = await get_href_values_xpath(session, url, xpath_expression)
+            print(f"Href values extracted using XPath (Page {page}):")
+            data_list = await process_links(session, base_url, href_list)
+            # Remove None elements from the data list (those are failed tasks)
+            data_list = [data for data in data_list if data is not None]
+            if data_list:
+                await write_data_to_json(data_list)
 
 
 def run_crawler():
@@ -88,11 +85,8 @@ def run_crawler():
 
     start_time = time.time()
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(crawl_website(
-        base_url, source_link,
-        headers=headers, xpath_expression=xpath_expression))
-    loop.close()
+    asyncio.run(crawl_website(base_url, source_link,
+                headers=headers, xpath_expression=xpath_expression))
 
     end_time = time.time()
     execution_time = end_time - start_time
